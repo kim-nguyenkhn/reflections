703d713b095167b5b32adabb5c23407aee315884
Aaron Lee <aaron.lee@gusto.com>
2024-11-04 15:23:35 -0500

    set header to sticky position

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { ThemeMode } from './ThemeMode'
 
 export default function Header() {
   return (
-    <div className='flex px-2 py-2 border-b shadow-sm border-neutral-200 dark:shadow-none dark:border-neutral-800'>
+    <div className='flex top-0 sticky px-2 py-2 border-b shadow-sm border-neutral-200 dark:shadow-none dark:border-neutral-800'>
         <ThemeMode/>
     </div>
   )
