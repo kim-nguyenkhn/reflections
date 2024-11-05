967d8b774965255610b71c085d5e8802e0c1b3d9
Aaron Lee <aaron.lee@gusto.com>
2024-11-05 13:04:56 -0500

    set max width on page container

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/components/ui/card";
 import { BasicInfo} from "@/components/BasicInfo";
 export default function Home() {
   return (
-    <div className="container mx-auto min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="container max-w-4xl mx-auto min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Card>
         <div className="p-6">
           <BasicInfo/>
